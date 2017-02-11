dbc13e64fa3eb344333378b3857d373cd28f50bb
Henner Kollmann <henner.kollmann@gmx.de>
2017-02-11 10:52:10 +0100

    Revert merge fix -handled in fix_merge branch
    revert qooxdoo path

--- source/class/com/zenesis/qx/upload/AbstractHandler.js
+++ source/class/com/zenesis/qx/upload/AbstractHandler.js
@@ -95,6 +95,17 @@ qx.Class.define("com.zenesis.qx.upload.AbstractHandler", {
     },
 
     /**
+     * Adds a blob to the upload list 
+     * 
+     * @param filename    {String}  name of object
+     * @param blob        {Blob}    the blob to upload
+     * @param params      {Object}  List of params added to the upload params
+     */
+    addBlob: function (filename, blob, params){
+      /* abstract */
+    },
+
+    /**
      * Adds a file to the outbound queue
      * 
      * @param file
