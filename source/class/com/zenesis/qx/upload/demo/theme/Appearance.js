4ba972a011a2eb4088464c46483226852dced069
Henner Kollmann <henner.kollmann@gmx.de>
2019-09-21 20:44:14 +0200

    move demo to main source part 2

---
+++ source/class/com/zenesis/qx/upload/demo/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("com.zenesis.qx.upload.demo.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ source/class/com/zenesis/qx/upload/demo/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("com.zenesis.qx.upload.demo.theme.Theme",
+{
+  meta :
+  {
+    color : com.zenesis.qx.upload.demo.theme.Color,
+    decoration : com.zenesis.qx.upload.demo.theme.Decoration,
+    font : com.zenesis.qx.upload.demo.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : com.zenesis.qx.upload.demo.theme.Appearance
+  }
+});
